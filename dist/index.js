dc02f95a86555452b67e64d7d58d2447f16ec00e
Callum Law <callumlaw1709@outlook.com>
2018-12-09 23:52:07 +1300

    Updated to class based syntax

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,16 @@
 const ChildProcess = require("child_process");
 const ServiceManagerBinary = "\"" + __dirname + "\\binaries\\ServiceManager.exe\"";
 
-module.exports = function(serviceName = ""){
-	if(require("os").platform().indexOf("win32") == -1){
-		throw "Service Manager is only supported on Microsoft Windows";
+module.exports = class ServiceManager{
+	constructor(ServiceName){
+		if(require("os").platform().indexOf("win32") == -1){
+			throw "Service Manager is only supported on Microsoft Windows";
+		}
+
+		this.ServiceName = serviceName;
 	}
 
-	this.ServiceName = serviceName;
-
-	this.execute = (Command, JsonEncoded = false)=>{
+	execute(Command, JsonEncoded = false){
 		return new Promise((resolve, reject)=>{
 			ChildProcess.exec(Command, {}, (err, stdout, stderr)=>{
 				if(err)
@@ -20,39 +22,39 @@ module.exports = function(serviceName = ""){
 				resolve(JsonEncoded ? JSON.parse(stdout) : stdout);
 			});
 		});
-	};
+	}
 
-	this.changeStartupType = (desiredStartupType)=>{
+	changeStartupType(desiredStartupType){
 		if(["auto", "boot", "disabled", "demand", "system"].indexOf(desiredStartupType.toLowerCase()) == - 1)
-			console.log("Invalid Startup Type");
+			throw "Invalid Startup Type";
 
 		return this.execute(["\"" + __dirname + "\\binaries\\SetStartupType.bat\"", this.ServiceName, desiredStartupType.toLowerCase()].join(" "));
-	};
+	}
 
-	this.continue = ()=>{
+	continue(){
 		return this.execute([ServiceManagerBinary, "Continue", this.ServiceName].join(" "));
-	};
+	}
 
-	this.getInfo = ()=>{
+	getInfo(){
 		return this.execute([ServiceManagerBinary, "GetInfo", this.ServiceName].join(" "), true);
-	};
+	}
 
-	this.pause = ()=>{
+	pause(){
 		return this.execute([ServiceManagerBinary, "Pause", this.ServiceName].join(" "));
-	};
+	}
 
-	this.start = ()=>{
+	start(){
 		return this.execute([ServiceManagerBinary, "Start", this.ServiceName].join(" "));
-	};
+	}
 
-	this.stop = ()=>{
+	stop(){
 		return this.execute([ServiceManagerBinary, "Stop", this.ServiceName].join(" "));
-	};
+	}
 
-	this.waitForStatus = (desiredStatus, Callback)=>{
+	waitForStatus(desiredStatus){
 		if(["ContinuePending", "Paused", "PausePending", "Running", "StartPending", "Stopped", "StopPending"].indexOf(desiredStatus) == -1)
 			throw "Invalid Status";
-		
+
 		return this.execute([ServiceManagerBinary, "WaitForStatus", this.ServiceName, desiredStatus].join(" "));
-	};
-};
+	}
+}
